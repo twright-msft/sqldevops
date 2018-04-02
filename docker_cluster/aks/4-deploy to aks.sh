333d334b569a80a01b4ac20047c311181a2fb882
Travis Wright <twright@microsoft.com>
2018-04-02 13:39:52 -0700

    one more change

--- docker_cluster/aks/4-deploy to aks.sh
+++ docker_cluster/aks/4-deploy to aks.sh
@@ -4,7 +4,7 @@ kubectl apply -f namespace.yml
 #create a persistent volume claim
 kubectl apply -f pvc.yml
 
-#wait for this to return that the PVC is BOUND before continuing!
+#wait for this to return that the PVC is BOUND before continuing!  Typically takes 1 minute.
 kubectl get pvc -n production
 
 #create the SQL Server stateful set in the production namespace
@@ -13,6 +13,6 @@ kubectl apply -f sql-statefulset.yml
 #create the web app in the production namespace
 kubectl apply -f webapp.yml
 
-#show the pods and services
+#show the pods and services.  WAIT for pods to finish deploying.  SQL pod takes 4-5 minutes.
 kubectl get pods -n production
 kubectl get services -n production
